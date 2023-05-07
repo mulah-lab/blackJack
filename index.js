20389447decb73815fd1ec5e34309a85c8e14ad0
mulah-lab <123460564+mulah-lab@users.noreply.github.com>
2023-05-07 16:04:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstCard = 11
+let secondCard = 10
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+  console.log("not enough");
+}
+else if (sum > 21) {
+  console.log("too much");
+}
+else 
+  console.log("You win!!");
+
